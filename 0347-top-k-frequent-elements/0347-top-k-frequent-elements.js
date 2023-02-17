60656a4fbd24b1e9ca43458c22e8b3b58f47586b
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-02-16 21:39:47 -0500

    Time: 81 ms (75.02%), Space: 46.3 MB (34.33%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -8,17 +8,16 @@ var topKFrequent = function(nums, k) {
     const bucket = [];
     const result = [];
     
-    for(let num of nums) {
+    for (const num of nums) {
         freqMap.set(num, (freqMap.get(num) || 0) + 1);
     }
     
-    for(let [num, freq] of freqMap) {
+    for (const [num, freq] of freqMap) {
         bucket[freq] = (bucket[freq] || new Set()).add(num);
     }
-
+    
     for (let i = bucket.length - 1; i >= 0; i--) {
         if (bucket[i]) result.push(...bucket[i]);
-        if (result.length === k) break;
+        if (result.length === k) return result;
     }
-    return result;
 };
