e850cd134bfabeabce87c57dc86316752d385999
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-02-21 16:49:12 -0500

    Time: 73 ms (48.83%), Space: 45.1 MB (38.52%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let idx = -1
+    
+    for (let i = 0; i < nums.length; i++){
+        if (nums[i] === target) {
+            return i
+        }
+    }
+    
+    return idx
+};
