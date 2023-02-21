2fc687921353e5da33664f96d3db68477666053c
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-02-21 16:09:07 -0500

    Time: 66 ms (95.34%), Space: 45.8 MB (31.02%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,45 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function(tokens) {
+     if (tokens.length === 1) return parseInt(tokens[0]);
+    let result = 0;
+    const stack = [];
+    const operatorMap = {
+    '+': 0,
+    '-': 0,
+    '*': 0,
+    '/': 0,
+    };
+
+    for (const val of tokens) {
+    if (val in operatorMap) {
+      if (stack.length > 1) {
+         const num1 = stack.pop();
+        const num2 = stack.pop();
+
+        result = helperFunction(num1, num2, val);
+        stack.push(result);
+      }
+    } else {
+      stack.push(parseInt(val));
+    }
+    }
+
+    return parseInt(result);
+    };
+
+
+const helperFunction = (num1, num2, operator) => {
+    switch ((num1, num2, operator)) {
+    case '+':
+      return num2 + num1;
+    case '-':
+      return num2 - num1;
+    case '*':
+      return num2 * num1;
+    case '/':
+      return parseInt(num2 / num1);
+    }
+};
