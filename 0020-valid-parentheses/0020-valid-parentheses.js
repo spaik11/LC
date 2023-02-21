e45b855a19f4daaadb554c6719fdd29dff54c953
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-02-20 22:07:35 -0500

    Time: 60 ms (92.47%), Space: 42.4 MB (62.28%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const map = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    const stack = [];
+    
+    for (const item of s) {
+        if (item in map) {
+            if (stack.length > 0 && stack[stack.length - 1] === map[item]) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        } else {
+            stack.push(item);
+        }
+    }
+    return stack.length === 0 ? true : false;
+};
