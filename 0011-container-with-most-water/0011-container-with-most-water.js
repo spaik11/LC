2b80ea7550a54ccf45d424a9561faa303208d61a
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-03-05 22:04:44 -0500

    Time: 78 ms (73.82%), Space: 49.8 MB (20.35%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+     let left = 0,
+    right = height.length - 1,
+    largestArea = 0;
+
+  while (left < right) {
+    const wallHeight = Math.min(height[right], height[left]);
+    largestArea = Math.max(wallHeight * (right - left), largestArea);
+
+    if (height[left] <= height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return largestArea;
+};
