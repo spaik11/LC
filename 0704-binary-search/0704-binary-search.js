ccd42af737a1f8c962f7b43e43bafe1ca6914f99
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-03-13 20:26:13 -0400

    Time: 62 ms (83.94%), Space: 45.3 MB (14.27%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,21 +4,16 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0;
-      let right = nums.length - 1;
-
-      while (left <= right) {
-        let mid = Math.floor((left + right) / 2);
-          
-        if (nums[mid] === target) { 
-          return mid;
-        } else if (nums[mid] < target) { 
-
-          left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-      }
+    let left = 0, right = nums.length - 1;
     
-      return -1;
+    while (left < right) {
+        let mid = left + Math.floor((right - left + 1) / 2);
+        
+        if (target < nums[mid]) {
+            right = mid - 1
+        } else {
+            left = mid; 
+        }
+    }
+    return nums[left] === target ? left : -1;
 };
