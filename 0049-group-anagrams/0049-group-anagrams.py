f55c62376db3c42ac14351e399185f8366320508
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-04-03 19:24:22 -0400

    Time: 97 ms (79.69%), Space: 17 MB (94.01%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,14 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagramDict = {}
+        result = []
+        
+        for str in strs:
+            anagramKey = ''.join(sorted(str))
+
+            if anagramKey in anagramDict:
+                anagramDict[anagramKey].append(str)
+            else:
+                anagramDict[anagramKey] = [str]
+                
+        return list(anagramDict.values())
