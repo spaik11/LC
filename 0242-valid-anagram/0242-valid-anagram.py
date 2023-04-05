1ea4064d2bcae35505da93508660ed80f53aa713
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-04-05 15:31:44 -0400

    Time: 46 ms (78.45%), Space: 14.4 MB (59.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        sHash = {}
+        for c in s:
+            sHash[c] = 1 + sHash.get(c, 0)
+        
+        tHash = {}
+        for c in t:
+            tHash[c] = 1 + tHash.get(c, 0)
+        
+        return sHash == tHash
