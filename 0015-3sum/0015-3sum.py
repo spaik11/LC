a7dba97cc311e07b7c676b7edab525aa31c2eead
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-06-18 14:32:54 -0400

    Time: 1442 ms (67.77%), Space: 20.4 MB (62.14%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -6,8 +6,7 @@ class Solution:
         for i, v in enumerate(nums):
             if i > 0 and v == nums[i - 1]:
                 continue
-            l = i + 1
-            r = len(nums)  - 1
+            l, r = i + 1, len(nums)  - 1
             while l < r:
                 triplet = v + nums[l] + nums[r]
                 if triplet == 0:
