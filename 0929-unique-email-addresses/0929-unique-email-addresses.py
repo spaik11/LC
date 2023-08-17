f8cbc7c1502369ba0dc57d3653c12d60fa50bd16
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-08-17 19:31:09 -0400

    Time: 56 ms (91.06%), Space: 16.6 MB (16.58%) - LeetHub

--- 0929-unique-email-addresses/0929-unique-email-addresses.py
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -2,11 +2,6 @@ class Solution:
     def numUniqueEmails(self, emails: List[str]) -> int:
         unique = set()
         
-        # split at @
-        # if left side includes . or +, calculate and put it in hash map
-        
-        # then check right side and see if its the same?
-        
         for e in emails:
             local, domain = e.split("@")
             local = local.split("+")[0].replace(".", "")
