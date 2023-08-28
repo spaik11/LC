b13b3f2df0360957fdd8b49ca8bfeb929b7f1708
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-08-28 14:45:41 -0400

    Time: 144 ms (93.06%), Space: 17.8 MB (95.54%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,10 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        hashM = {}
+        count, res = 0, 0
         
         for n in nums:
-            hashM[n] = hashM.get(n, 0) + 1
-            
-        return max(hashM, key=hashM.get)
+            if count == 0:
+                res = n
+            count += (1 if n == res else -1)
+                
+        return res
