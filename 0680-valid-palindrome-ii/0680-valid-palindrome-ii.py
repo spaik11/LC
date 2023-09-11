200417027573b609d68c5dddc2a2ce87526c8c36
Sung Paik <54951465+spaik11@users.noreply.github.com>
2023-09-10 21:55:02 -0400

    Time: 116 ms (68.56%), Space: 16.9 MB (75.47%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,11 +1,20 @@
 class Solution:
+    def helper(self,s,p1,p2) :
+        while p1 < p2 :
+            if s[p1] != s[p2]:
+                return False
+            
+            p1 += 1
+            p2 -= 1
+        
+        return True
+    
     def validPalindrome(self, s: str) -> bool:
         l, r = 0, len(s) - 1
         
         while l < r:
             if s[l] != s[r]:
-                skipL, skipR = s[l + 1:r + 1], s[l:r]
-                return (skipL == skipL[::-1] or skipR == skipR[::-1])
+                return (self.helper(s, l + 1 ,r) or self.helper(s, l, r - 1))
             
             l, r = l + 1, r - 1
             
